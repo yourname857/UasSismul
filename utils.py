cc56f248e427de1fa62f3a56af6a6a2a46997875
pratama-13 <zexxach@gmail.com>
2025-06-26 23:37:22 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,12 @@
+import os
 import subprocess
 
+# Path ke ffmpeg
+ffmpeg_path = os.path.join("bin", "ffmpeg.exe")
 
 def encode_intra_frame(input_path, output_path):
     result = subprocess.run([
-        'ffmpeg', '-i', input_path,
+        ffmpeg_path, '-i', input_path,
         '-g', '1', '-c:v', 'libx264', '-preset', 'fast', '-crf', '23',
         output_path
     ], capture_output=True, text=True)
@@ -11,22 +14,22 @@ def encode_intra_frame(input_path, output_path):
 
 def encode_inter_frame(input_path, output_path):
     result = subprocess.run([
-        'ffmpeg', '-i', input_path,
+        ffmpeg_path, '-i', input_path,
         '-g', '30', '-c:v', 'libx264', '-preset', 'fast', '-crf', '23',
         output_path
     ], capture_output=True, text=True)
     print(result.stderr)
 
-def convert_format(input_path, output_path, format):
+def convert_format(input_path, output_path):
     result = subprocess.run([
-        'ffmpeg', '-i', input_path,
+        ffmpeg_path, '-i', input_path,
         output_path
     ], capture_output=True, text=True)
     print(result.stderr)
 
 def compress_video(input_path, output_path):
     result = subprocess.run([
-        'ffmpeg', '-i', input_path,
+        ffmpeg_path, '-i', input_path,
         '-vf', 'scale=640:-1',
         '-b:v', '500k', '-c:v', 'libx264', '-preset', 'fast',
         output_path
